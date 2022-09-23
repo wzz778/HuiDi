c2bfaa05443ffad75084dae7912a1df04a2e02fa
wangzezheng <2295908251@qq.com>
2022-09-23 15:36:36 +0800

    wzz9.23.1

--- router/userRouter.js
+++ router/userRouter.js
@@ -6,4 +6,11 @@ axios.default.baseURL = 'http://110.40.205.103:8099/';
 router.get('/layout',(req,res)=>{
     res.render('layout.html')
 })
+router.get('/Personalhomepage',(req,res)=>{
+    res.render('Personalhomepage.html')
+})
+router.get('/login',(req,res)=>{
+    res.render('login.html')
+})
 module.exports=router;
+

2697bc3e44462d542307c1f6b41d6c76be0e3bfa
wangzezheng <2295908251@qq.com>
2022-09-24 11:24:44 +0800

    wzz 9.24.11

--- router/userRouter.js
+++ router/userRouter.js
@@ -2,7 +2,6 @@ const express = require('express');
 const axios = require("axios");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
-axios.default.baseURL = 'http://110.40.205.103:8099/';
 router.get('/layout',(req,res)=>{
     res.render('layout.html')
 })
@@ -11,6 +10,82 @@ router.get('/Personalhomepage',(req,res)=>{
 })
 router.get('/login',(req,res)=>{
     res.render('login.html')
+})
+router.get('/register',(req,res)=>{
+    res.render('register.html')
+})
+router.get('/register2',(req,res)=>{
+    res.render('register2.html')
+})
+axios.defaults.baseURL = 'http://152.136.99.236:8080/'
+//发送验证码
+router.post('/api/sendcode', (req, res) => {    
+    var mailLimit = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+    var mailJudge = mailLimit.test(req.body.mail);
+    if (mailJudge==false){
+        res.send({ err: -1, msg:'邮箱形式错误'});
+    }else{
+        axios({
+            url:'/loginAndRegister/sendMail',
+            method:'post',
+            params:req.body,
+        }).then(response=>{
+            console.log(response.data);
+            if(response.data.msg=='OK'){
+                res.send({ err: 0, msg:"OK"});
+            }else if(response.data.msg=='已有该用户'){
+                res.send({ err: -1, msg:'已有该用户'});
+            }else{
+                res.send({ err: -1, msg:'数据操作错误'});
+            }
+        }).catch(function (error) {
+            console.log(error);
+            res.send({ err: -1, msg: '网络错误' })
+        });
+    }
+})
+//检查验证码
+router.post('/api/checkcode', (req, res) => {    
+    var mailLimit = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+    var mailJudge = mailLimit.test(req.body.mail);
+    if (mailJudge==false){
+        res.send({ err: -1, msg:'邮箱形式错误'});
+    }else{
+        axios({
+            url:'/loginAndRegister/checkCode',
+            method:'post',
+            params:req.body,
+        }).then(response=>{
+            console.log(response.data);
+            if(response.data.msg=='OK'){
+                res.send({ err: 0, msg:"OK"});
+            }else{
+                res.send({ err: -1, msg:'数据操作错误'});
+            }
+        }).catch(function (error) {
+            console.log(error);
+            res.send({ err: -1, msg: '网络错误' })
+        });
+    }
+})
+//注册
+router.post('/api/register', (req, res) => {    
+    axios({
+        url:'/loginAndRegister/register',
+        method:'post',
+        params:req.body,
+    }).then(response=>{
+        console.log(response.data);
+        if(response.data.msg=='OK'){
+            res.send({ err: 0, msg:"OK"});
+        }else{
+            res.send({ err: -1, msg:'数据操作错误'});
+        }
+    }).catch(function (error) {
+        console.log(error);
+        res.send({ err: -1, msg: '网络错误' })
+    });
+    
 })
 module.exports=router;
 
---
+++ static/js/register2.js
@@ -0,0 +1,79 @@
+window.onload = function() {
+	createCode();
+}
+function isnull(val) {
+    var str = val.replace(/(^\s*)|(\s*$)/g, '');//去除空格;
+    if (str == '' || str == undefined || str == null) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+let mymail=document.getElementById('mymail');
+let password=document.getElementById('password');
+let repassword=document.getElementById('repassword');
+let username=document.getElementById('username');
+function createCode() {
+	code = "";
+	var codeLength = 4; //验证码的长度   
+	var checkCode = document.getElementById("checkCode");
+	var random = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'); //随机数   
+	for(var i = 0; i < codeLength; i++) { //循环操作   
+		var charIndex = Math.floor(Math.random() * 36); //取得随机数的索引   
+		code += random[charIndex]; //根据索引取得随机数加到code上   
+	}
+	checkCode.value = code; //把code值赋给验证码   
+}
+//校验验证码   
+function validate() {
+	var inputCode = document.getElementById("input").value.toUpperCase(); //取得输入的验证码并转化为大写         
+	if(inputCode.length <= 0) { //若输入的验证码长度为0   
+		alert("请输入验证码！"); //则弹出请输入验证码   
+		return
+	} else if(inputCode != code) { //若输入的验证码与产生的验证码不一致时   
+		alert("验证码输入错误！"); //则弹出验证码输入错误   
+		createCode(); //刷新验证码   
+		return
+	} 
+	let pa=password.value;
+	let repa=repassword.value;
+	let name=username.value
+	if(isnull(pa)||isnull(repa)||isnull(name)){
+		alert("请填写完整内容")
+		return 
+	}
+	if(name.length)
+	if(pa!=repa){
+		alert("您输入的两次密码不一致")
+		return 
+	}else{
+		axios({
+            url: '/api/register',
+            method: 'post',
+            data: {
+              "mail": mail,
+			  "password":pa,
+			  "username":username
+            }
+          }).then(data => {
+            console.log(data.data);
+            if(data.data.err==0){
+                alert('发送成功');
+                opendjs()
+            }else{
+                alert(data.data.msg);
+            }
+          }).catch(function (error) {
+            
+          });
+	}
+}
+if( sessionStorage.getItem('mymail')){
+	mymail.innerHTML=sessionStorage.getItem('mymail');
+}else{
+	alert('请先验证您的邮箱');
+	setTimeout(function () {
+		window.location.assign("/register");
+	}, 300)
+}

9a3fb6abf12b364042598a311c908608dd11aef8
wangzezheng <2295908251@qq.com>
2022-10-05 17:00:21 +0800

    wzz完成了更改邮箱，观看粉丝，关注页面

---
+++ static/js/reemail.js
@@ -0,0 +1,117 @@
+let mypassword=document.getElementById('mypassword');
+let mymessage=document.getElementsByClassName('mymessage');
+function checkpassword(){
+    if(isnull(mypassword.value)){
+        alert("请输入密码!");
+        return 
+    }
+    axios({
+        url: '/api/checkaccount',
+        method: 'get',
+        params:{
+            password:mypassword.value
+        }
+      }).then(data => {
+        // console.log(data.data);
+        if(data.data.err==0){   
+            alert("成功！");
+            // sessionStorage.setItem('havecheck', 1);
+            mymessage[0].style.display='none';
+            mymessage[1].style.display='block';
+        }else{
+            alert("失败！");
+        }
+      })
+      .catch(function (error) {
+        alert("失败！");
+        console.log(error);
+      });
+}
+let mymail=document.getElementById('mymail')
+let getcodea=document.getElementById('getcodea')
+let mycode=document.getElementById('mycode')
+var times=60;
+var djs
+function opendjs(){
+    djs=setInterval(function(){
+        if(times==0){
+            times=60;
+            clearInterval(djs)
+             getcodea.innerHTML=`获取验证码`
+             getcodea.style.cursor='pointer';
+             getcodea.onclick=getcode;
+        }else{
+            getcodea.style.cursor='not-allowed';
+            getcodea.onclick=function(){return};
+            getcodea.innerHTML=`${times--}s后可重新发送`
+        }
+    },1000);
+
+}
+function getcode(){
+    let mail=mymail.value;
+    if(isnull(mail)){
+        alert('请输入您要修改的邮箱');
+        return
+    }else{
+        axios({
+            url: '/api/sendcode',
+            method: 'post',
+            data: {
+              "mail": mail,
+            }
+          }).then(data => {
+            console.log(data.data);
+            if(data.data.err==0){
+                alert('发送成功');
+                opendjs()
+            }else{
+                alert(data.data.msg);
+            }
+          }).catch(function (error) {
+            
+          });
+    }
+}
+function checkcode(){
+    return new Promise((resolve,reject)=>{
+        let mail=mymail.value;
+        let code=mycode.value;
+        if(isnull(mail)){
+            reject('请输入您要修改的邮箱');
+        }else if(isnull(code)){
+            reject('请输入验证码');
+        }else{
+            axios({
+                url: '/api/checkcode',
+                method: 'post',
+                data: {
+                  "mail": mail,
+                  'code':code
+                }
+              }).then(data => {
+                console.log(data.data);
+                if(data.data.err==0){
+                    alert('验证成功');
+                    resolve()
+                    // sessionStorage.setItem('mymail',mail);
+                    // sessionStorage.removeItem('Applicationid');
+                    // setTimeout(function () {
+                    //     window.location.assign("/Personalhomepage");
+                    // }, 300)
+                }else{
+                    reject(data.data.msg);
+                }
+              })
+        }
+    
+    }).then(data2=>{
+        alert("验证成功！")
+        setTimeout(function () {
+            window.location.assign("/Personalhomepage");
+        }, 300)
+      })
+      .catch(function (error) {
+          alert(error)    
+      });
+}
--- static/js/userhomepage.js
+++ static/js/userhomepage.js
@@ -1,16 +1,41 @@
 let textmessage=document.getElementsByClassName('textmessage');
 let conli=document.getElementsByClassName('conli');
 let cordimg=document.getElementById('cordimg')
-let seximg=document.getElementsByClassName('seximg')
+let seximg=document.getElementsByClassName('seximg')[0]
 let cord_contentHead=document.getElementById('cord-contentHead')
 let useremail=document.getElementsByClassName('useremail')
 let mynumber=document.getElementsByClassName('mynumber')
 let class_body1=document.getElementsByClassName('class_body')[0]
-let class_body2=document.getElementsByClassName('albumbody')[0];
+let albumbody=document.getElementById('albumbody');
 let album=document.getElementById('album')
+let dynumber=document.getElementById('dynumber')
+let lookmore=document.getElementById('lookmore')
+let lookend=document.getElementById('lookend')
 let album_input=document.getElementsByClassName('album_input');
 let followbutton=document.getElementsByClassName('followbutton')
 let userid = window.location.search.split("=")[1];
+let mymessageback=document.getElementById('mymessageback')
+axios({
+    url: '/api/isuser',
+    method: 'get',
+    params:{
+        id:userid
+    }
+  }).then(data => {
+    if(data.data.err==0){
+       if(data.data.msg){
+            window.location.assign("/Personalhomepage");
+       }else{
+        return
+       }
+    }else{
+        // alert("未登录")
+        return 
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 function changeclass(i){
     conli[i].classList.add('havebethis');
     textmessage[i].style.display='block';
@@ -28,16 +53,18 @@ axios({
         id:userid
     }
   }).then(data => {
-    // console.log(data.data);
     if(data.data.err==0){
         let me=data.data.msg;
         if(me.img_url!=null){
             cordimg.style.backgroundImage=`url(${me.img_url})`;
         }
+        if(me.background_img!=null){
+            mymessageback.style.backgroundImage=`url(${me.background_img})`;
+        }
         cord_contentHead.innerHTML=me.name;
         useremail[0].innerHTML=me.mail;
         if(me.sex!="男"){
-            seximg.src=`url(/public/img/woman.png)`;
+            seximg.src=`/public/img/woman.png`;
         }
         return axios({url: '/api/getusernumber',method: 'get', params:{
             id:userid
@@ -47,7 +74,6 @@ axios({
         
     }
   }).then(data1 => {
-    // console.log(data1);
     let number=data1.data.msg;
     mynumber[0].innerHTML=number.关注数;
     mynumber[1].innerHTML=number.粉丝数;
@@ -55,72 +81,26 @@ axios({
   .catch(function (error) {
     console.log(error);
   });
-  function showmydynamic(){
-      axios({
-        url: '/api/getuserdynamic',
-        method: 'get',
-        params:{
-            size:10,
-            begin:0,
-            id:userid
-        }
-      }).then(data => {
-        if(data.data.err==0){
-            let arr=data.data.msg.records;
-            class_body1.innerHTML=``
-            for(let i in arr){
-                let arrimg='';
-                for(let n of arr[i].img_url){
-                    arrimg+=`<img src="${n}" alt="">`
-                }
-                class_body1.innerHTML+=`
-                <div class="dynamicmax">
-                    <div class="user_cord">
-                        <a href="" class="dyusera">
-                            <img src="${arr[i].user_img_url}" class="dyuserhead" alt="">
-                            <span class="dyusername">${arr[i].user_name}</span>
-                            <span class="dyuserid">${arr[i].user_id}</span>
-                        </a>
-                        <span class="dytime">${arr[i].create_time}</span>
-                    </div>
-                    <a href="" class="dytexta">
-                        <div class="imgde">
-                        ${arr[i].describes}
-                        </div>
-                        <div class="imgmax">
-                        ${arrimg}
-                        </div>
-                    </a>
-                </div>
-                `
-            }
-        }else{
-            // alert("未登录")
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-  showmydynamic()
 function getmyalbum(){
     axios({
         url: '/api/getuseralbumname',
         method: 'get',
         params:{
-            id:userid
+            id:userid,
+            size:10,
+            begin:1
         }
       }).then(data => {
         // console.log(data.data);
         if(data.data.err==0){
-            let allarr=data.data.msg;
-            class_body2.innerHTML=``;
+            let allarr=data.data.msg.list;
+            albumbody.innerHTML=``;
             for(let i in allarr){
+                // console.log(allarr[i]);
                 let arrimg=allarr[i].images;
-                // console.log(arrimg);
                 if(JSON.stringify(arrimg)=="{}"){
-                    class_body2.innerHTML+=`
-                    <a href="javascript:;" class="Aalbummax">
+                    albumbody.innerHTML+=`
+                    <a href="album?albumId=${allarr[i].album.id}" class="Aalbummax">
                         <span class="albumid">${allarr[i].album.id}</span>
                         <div class="imgbox">
                         <div class="img" style="background-image:url(/public/img/kongalbum.png);"></div>
@@ -133,7 +113,7 @@ function getmyalbum(){
                         </div>
                     </a>
                     `;
-                    return 
+                    continue
                 }
                 // console.log(arrimg);
                 let allimg=``;
@@ -141,22 +121,28 @@ function getmyalbum(){
                 let imgnumber=0;
                 for(let key in arrimg){
                      thisimg=arrimg[key];
+                     console.log(key);
                      imgnumber+=arrimg[key].length;
                 }
-                // console.log(thisimg);
+                // console.log(thisimg);   
                 let imglength=thisimg.length;
                 if(imglength==1){
                     allimg=`
                         <div class="img" style="background-image:url(${thisimg[0].img_url});"></div>
                     `
-                }else{
+                }else if(imglength==0){
+                    allimg=`
+                        <div class="img" style="background-image:url(public/img/userHead.jpg);"></div>
+                    `
+                }
+                else{
                     allimg=`
                     <div class="img2" style="background-image:url(${thisimg[0].img_url});"></div>
                     <div class="img2" style="background-image:url(${thisimg[1].img_url});"></div>
                 `
                 }
-                class_body2.innerHTML+=`
-                <a href="" class="Aalbummax">
+                albumbody.innerHTML+=`
+                <a href="album?albumId=${allarr[i].album.id}" class="Aalbummax">
                     <span class="albumid">${allarr[i].album.id}</span>
                     <div class="imgbox">
                         ${allimg}
@@ -171,10 +157,10 @@ function getmyalbum(){
                 `;
             }
         }else{
-
+            console.log(data.data);
         }
       }).catch(function (error) {
-        
+        console.log(error);
       });
 }
 function tofollow(){
@@ -185,7 +171,7 @@ function tofollow(){
             userid:userid
         }
       }).then(data => {
-        console.log(data.data);
+        // console.log(data.data);
         if(data.data.err==0){
             if(data.data.msg=='success'){
                 followbutton[0].style.display='none'
@@ -210,7 +196,7 @@ function outfollow(){
             userid:userid
         }
       }).then(data => {
-        console.log(data.data);
+        // console.log(data.data);
         if(data.data.err==0){
             if(data.data.msg=='success'){
                 followbutton[1].style.display='none'
@@ -235,7 +221,7 @@ function inspectfollow(){
             userid:userid
         }
       }).then(data => {
-        console.log(data.data);
+        // console.log(data.data);
         if(data.data.err==0){
             if(data.data.msg){
                 followbutton[0].style.display='none'
@@ -253,3 +239,74 @@ function inspectfollow(){
       });
 }
 inspectfollow()
+let dynowpage=1;
+let dyallpage=1;
+function showmydynamic(){
+    axios({
+      url: '/api/getuserdynamic',
+      method: 'get',
+      params:{
+          size:5,
+          begin:dynowpage,
+          id:userid
+      }
+    }).then(data => { 
+      console.log(data.data.msg);
+      if(data.data.err==0){
+          dynumber.innerText=data.data.msg.all_count
+          dyallpage=data.data.msg.all_page;
+          let arr=data.data.msg.list;
+        //   class_body1.innerHTML=``
+          for(let i in arr){
+              let arrimg='';
+              for(let n of arr[i].img_url){
+                  arrimg+=`<img src="${n}" alt="">`
+              }
+              class_body1.innerHTML+=`
+              <div class="dynamicmax">
+                  <div class="user_cord">
+                      <a href="userhomepage?id=${arr[i].user_id}" class="dyusera">
+                          <img src="${arr[i].user_img_url}" class="dyuserhead" alt="">
+                          <span class="dyusername">${arr[i].user_name}</span>
+                          <span class="dyuserid">${arr[i].user_id}</span>
+                      </a>
+                      <span class="dytime">${arr[i].create_time}</span>
+                  </div>
+                  <a href="dynamicDetails?id=${arr[i].img_id}" class="dytexta">
+                      <div class="imgde">
+                      ${arr[i].describes}
+                      </div>
+                      <div class="imgmax">
+                      ${arrimg}
+                      </div>
+                  </a>
+              </div>
+              `
+          }
+      }else{
+          // alert("未登录")
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+showmydynamic()
+window.onscroll = function () {
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    let windowHeight = document.documentElement.clientHeight || document.body.clientHeight;
+    let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+    let that = this;
+    if (scrollHeight - 1<= scrollTop + windowHeight) {
+        if(dyallpage>dynowpage){
+            dynowpage++;
+            lookmore.style.display='block';
+            lookend.style.display='none';
+            setTimeout(function(){showmydynamic()},1500)
+            // showmydynamic()
+        }else{
+            lookmore.style.display='none';
+            lookend.style.display='block';
+        }
+    }
+};

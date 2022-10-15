036532648daf0d446fc41f5890dac1e37ac8bcf8
XY <3400164864@qq.com>
2022-10-15 15:05:54 +0800

    配置404

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(session({
   secret: 'hubwizApp',
   cookie: { maxAge: 60 * 1000 * 30 * 24 },
   resave: true,
-  saveUninitialized: false, 
+  saveUninitialized: false,
 }))
 app.use('/public', express.static(path.join(__dirname, './static')))
 app.use('/node_modules', express.static(path.join(__dirname, './node_modules')));
@@ -26,14 +26,17 @@ app.use(cors())
 app.set("views", "views");
 // 引入路由
 const userRouter = require('./router/userRouter')
-const liRouter=require('./router/liRouter')
+const liRouter = require('./router/liRouter')
 const zllRouter = require('./router/zllRouter');
 const userloginRouter = require('./router/userloginRouter');
+const errRouter = require('./router/errRouter')
 // 配置路由
 app.use(liRouter)
 app.use(userRouter);
 app.use(userloginRouter);
 app.use(zllRouter);
+app.use(errRouter)
+
 // 启动服务器
 app.listen(8099, () => {
   console.log('服务器已启动,端口号:8099')
